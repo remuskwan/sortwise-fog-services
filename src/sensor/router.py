6e2604dbcd8d8d3c6fc4223dd380720048da8192
Remus <remuskwan23@gmail.com>
2024-04-08 17:04:28 +0800

    chore: cleanup comments

--- src/sensor/router.py
+++ src/sensor/router.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter
-from .distance import router as distance_router
 
 router = APIRouter(
     prefix="/sensor",
     tags=["sensor"],
     responses={404: {"description": "Not found"}},
 )
-
-# router.include_router(distance_router)
