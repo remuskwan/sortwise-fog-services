758ef24699aff5da778a99f40948dbd5a8c3b61a
Remus <remuskwan23@gmail.com>
2024-04-08 17:03:11 +0800

    feat: publish endpoint

--- src/sensor/distance/router.py
+++ src/sensor/distance/router.py
@@ -14,8 +14,10 @@ load_dotenv()
 MQTT_BROKER = os.getenv("MQTT_BROKER")
 MQTT_PORT = int(os.getenv("MQTT_PORT"))
 MQTT_TOPIC = os.getenv("MQTT_DISTANCE_TOPIC")
-
+MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
 
 @router.on_event("startup")
